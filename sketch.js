4fe37add3aee3d141edededc7c64cda8c8cab2b4
hatchVCode <75653400+hatchVCode@users.noreply.github.com>
2021-12-02 15:29:47 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  x = windowWidth / 2; 
+  var canvas = createCanvas(x, 100);
+  canvas.parent('sketch-holder');
+  background(255, 0, 200);
+}
+
+function draw() {
+  frameRate(180);
+  noStroke(); 
+  background(220, 230, 210);
+  fill(186, 128, 274);
+  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(mouseY, mouseX, 80, 80);
+  fill(86, 28, 74);
+  text("Hold a Ball Version 2", 20, 40); 
+}
+
